d5abbb0dca63500a851f1cf5da0f6dba413f5739
Owen Yang <82738322+owenyang2@users.noreply.github.com>
2025-01-16 00:41:38 -0500

    Add deletion to todo list

--- todo-script.js
+++ todo-script.js
@@ -4,11 +4,23 @@ const taskAddInput = document.getElementById("add-task-input");
 
 function createTask()
 {
-    taskList.insertAdjacentHTML("beforeend", `
-        <div class="task-item">
-            <span>${taskAddInput.value}</span>
-        </div>
-    `);
+    const taskItem = document.createElement("div");
+    taskItem.classList.add("task-item");
+    
+    const taskText = document.createElement("span");
+    taskText.innerText = taskAddInput.value;
+    
+    const taskButton = document.createElement("button");
+    taskButton.innerText = "X";
+
+    taskItem.insertAdjacentElement("beforeend", taskText);
+    taskItem.insertAdjacentElement("beforeend", taskButton);
+
+    taskList.insertAdjacentElement("beforeend", taskItem);
+
+    taskButton.addEventListener("click", () => {
+        taskItem.remove();
+    })
 }
 
 taskAddButton.addEventListener("click", createTask);
